fa9297fffeef6970372c915c9df9063112fe0623
Eldar RasuLov <morddiff@gmail.com>
2026-04-05 18:56:34 +0400

    Herbivores and carnivores 0.1

--- app/main.py
+++ app/main.py
@@ -1,13 +1,16 @@
 class Animal:
     alive = []
+
     def __init__(self, name, health=100, hidden=False):
         self.name = name
         self.health = health
         self.hidden = hidden
         Animal.alive.append(self)
 
+
     def __repr__(self):
         return f"{{Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}}}"
+
 
 class Herbivore(Animal):
     def hide(self):
@@ -20,4 +23,3 @@ class Carnivore(Animal):
             other.health -= 50
             if other.health <= 0:
                 Animal.alive.remove(other)
-
