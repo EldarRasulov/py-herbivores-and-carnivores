bf4e63e9f6613582931e3662666405545e9a50d5
Eldar RasuLov <morddiff@gmail.com>
2026-04-05 19:19:24 +0400

    Herbivores and carnivores 0.9

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 class Animal:
     alive = []
 
-    def __init__(self, name: str, health: int = 100, hidden: bool = False) -> None:
+    def __init__(self, name: str, health: int = 100, hidden: bool = False):
         self.name = name
         self.health = health
         self.hidden = hidden
