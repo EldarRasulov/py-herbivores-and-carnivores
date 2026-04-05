7f0fe5840336a3fd903f980e827e08fb6c848e27
Eldar RasuLov <morddiff@gmail.com>
2026-04-05 18:59:18 +0400

    Herbivores and carnivores 0.3

--- app/main.py
+++ app/main.py
@@ -1,13 +1,11 @@
 class Animal:
     alive = []
 
-
     def __init__(self, name, health=100, hidden=False):
         self.name = name
         self.health = health
         self.hidden = hidden
         Animal.alive.append(self)
-
 
     def __repr__(self):
         return f"{{Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}}}"
