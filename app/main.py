1730bdd6a01732d22ffaf2331977cb31ce33e1de
Eldar RasuLov <morddiff@gmail.com>
2026-04-05 19:05:43 +0400

    Herbivores and carnivores 0.6

--- app/main.py
+++ app/main.py
@@ -1,20 +1,14 @@
-from typing import List, Optional, Union
-
-
 class Animal:
-    alive: List['Animal'] = []
+    alive = []
 
-    def __init__(self, name: str, health: int = 100, hidden: bool = False) -> None:
+    def __init__(self, name, health=100, hidden=False):
         self.name = name
         self.health = health
         self.hidden = hidden
         Animal.alive.append(self)
 
-    def __repr__(self) -> str:
-        return (
-            f"{{Name: {self.name}, Health: {self.health}, "
-            f"Hidden: {self.hidden}}}"
-        )
+    def __repr__(self):
+        return f"{{Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}}}"
 
 
 class Herbivore(Animal):
@@ -23,7 +17,7 @@ class Herbivore(Animal):
 
 
 class Carnivore(Animal):
-    def bite(self, other: Animal) -> None:
+    def bite(self, other) -> None:
         if isinstance(other, Herbivore) and not other.hidden:
             other.health -= 50
             if other.health <= 0:
